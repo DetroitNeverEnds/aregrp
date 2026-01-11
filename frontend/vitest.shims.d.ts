5aae09d3e4a64f1778a00387e607edeaf8a4b6d9
ArtTemiy <47613619+ArtTemiy@users.noreply.github.com>
2026-01-11 23:56:45 +0400

    try fix publishing storybook

--- frontend/vitest.shims.d.ts
+++ frontend/vitest.shims.d.ts
@@ -1,2 +1,12 @@
-/// <reference types="vitest" />
-/// <reference types="@testing-library/jest-dom" />
+/* eslint-disable */
+/// <reference types="vitest/globals" />
+
+import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+
+declare module 'vitest' {
+    interface Assertion<T = any>
+        extends TestingLibraryMatchers<
+            ReturnType<typeof expect.stringContaining>,
+            T
+        > {}
+}
